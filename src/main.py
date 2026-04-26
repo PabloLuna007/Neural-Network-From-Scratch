714b64b18db5dab4b1eb40d3d7e8b15763e9d4d3
Pablo <Pablo>
2026-04-25 18:01:25 -0600

    started neural network class

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import scipy
+import matplotlib.pyplot as plt
+
+class Neural_Net():
+    def __init__(self,input_,weights):
+        self.weights = []
+        self.inputs_ = []
+
+    def init_weights(self,weight_num):
+        pass
+
